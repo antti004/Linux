310abd3a580fe8edb20d9a378d95b5632c4e876d
aaz-srv1 <antti.arrak@outlook.com>
2020-12-26 21:20:06 +0000

    Update

--- home/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-CUR_PATH=$(pwd)
-for f in .???*; do
-  rm $HOME/$f
-  ln -s $CUR_PATH/$f $HOME/$f
-done
--- install.home.sh
+++ install.home.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-#create symlinks for dot files
 
+cd home
+FILES_PATH=$(pwd)
+for f in .???*; do
+   echo $f
+  rm $HOME/$f
+  ln -s $FILES_PATH/$f $HOME/$f
+done
