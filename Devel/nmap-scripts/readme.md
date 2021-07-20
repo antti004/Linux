8e9a3aa7c588a316af2d04963485147dd57f752a
pi aa-pi1 <pi@aa-pi1.local>
2021-07-20 13:36:41 +0300

    Update

---
+++ Devel/nmap-scripts/readme.md
@@ -0,0 +1,13 @@
+### Open ports
+
+nmap 192.168.2.95
+
+## Specific ports
+nmap -p 1-200 <ip>
+
+## Scan all ports
+nmap -p- <ip>
+
+## Scan UPD ports
+nmap -p- -sU <ip>
+
--- first_install.sh
+++ first_install.sh
@@ -2,7 +2,7 @@
 clear
 apt-get update
 apt-get -y dist-upgrade
-apt-get install -y zsh htop vim git tmux apt-transport-https zip unzip software-properties-common multitail
+apt-get install -y zsh htop vim git tmux apt-transport-https zip unzip software-properties-common multitail exa
 sudo chsh -s $(which zsh) $USER
 
 sh -c "echo Europe/Tallinn > /etc/timezone"
