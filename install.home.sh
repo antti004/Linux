7fe66deb29df459adf9d8613403101227be82df8
Kasutaja <kasutaja@aa-4105.local>
2022-09-28 15:20:14 +0000

    Update home config files

--- install.home.sh
+++ install.home.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 
-cd home
+#cd home
 FILES_PATH=$(pwd)
 for f in .???*; do
    echo $f
   rm $HOME/$f
-  ln -s $FILES_PATH/$f $HOME/$f
+  #ln -s $FILES_PATH/$f $HOME/$f
+  ln -s Linux/$f $HOME/$f
 done
