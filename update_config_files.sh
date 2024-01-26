f7a7dd3042b4036ef427381b16e4be73a3dfb744
Antti Arrak <antti004@users.noreply.github.com>
2024-01-26 10:26:45 +0200

    Update update_config_files.sh

--- update_config_files.sh
+++ update_config_files.sh
@@ -3,3 +3,4 @@ wget https://github.com/antti004/Linux/raw/main/.nanorc -O .nanorc
 wget https://github.com/antti004/Linux/raw/main/.bash_profile -O .bash_profile
 wget https://github.com/antti004/Linux/raw/main/.tmux.conf -O .tmux.conf
 wget https://github.com/antti004/Linux/raw/main/update_config_files.sh -O update_config_files.sh
+
