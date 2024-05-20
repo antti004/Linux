403b1c52110cba81ff7325eba4be674e3cc930ce
Antti Arrak <antti004@users.noreply.github.com>
2024-05-20 14:30:11 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 wget https://github.com/antti004/Linux/raw/main/.nanorc -O .nanorc
 wget https://github.com/antti004/Linux/raw/main/.bash_profile -O .bash_profile
 wget https://github.com/antti004/Linux/raw/main/.tmux.conf -O .tmux.conf
-wget https://github.com/antti004/Linux/raw/main/update_config_files.sh -O update_config_files.sh
+wget https://github.com/antti004/Linux/raw/main/install.sh -O install.sh
 
