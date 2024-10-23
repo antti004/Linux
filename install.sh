d69cd6e8d28a771421bf3d4297e706e4687e8408
Antti Arrak <antti004@users.noreply.github.com>
2024-10-23 16:18:33 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,7 @@ wget https://github.com/antti004/Linux/raw/main/.bash_profile -O .bash_profile
 wget https://github.com/antti004/Linux/raw/main/.tmux.conf -O .tmux.conf
 wget https://github.com/antti004/Linux/raw/main/install.sh -O install.sh
 
+mkdir ~/.install_scripts
+echo "Reload...."
+source ~/.bashrc
+source ~/.bash_profile
