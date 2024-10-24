63364a491b7d8036cc6e39330946781e7735c233
Antti Arrak <antti004@users.noreply.github.com>
2024-10-24 09:19:27 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,3 +5,5 @@ wget https://github.com/antti004/Linux/raw/main/.tmux.conf -O .tmux.conf
 wget https://github.com/antti004/Linux/raw/main/install.sh -O install.sh
 
 mkdir -p ~/.installers
+
+wget https://dot.net/v1/dotnet-install.sh -O ~/.installers/dotnet-install.sh
