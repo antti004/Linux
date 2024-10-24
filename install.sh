5f1ed34819fbc351151bfa663bb63b2502a1c578
Antti Arrak <antti004@users.noreply.github.com>
2024-10-24 09:15:14 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,7 @@
-#/bin/bash
+#!/usr/bin/env bash shebang
 wget https://github.com/antti004/Linux/raw/main/.nanorc -O .nanorc
 wget https://github.com/antti004/Linux/raw/main/.bash_profile -O .bash_profile
 wget https://github.com/antti004/Linux/raw/main/.tmux.conf -O .tmux.conf
 wget https://github.com/antti004/Linux/raw/main/install.sh -O install.sh
 
-mkdir ~/.install_scripts
-echo "Reload...."
-source ~/.bashrc
-source ~/.bash_profile
+mkdir -p ~/.install_scripts
