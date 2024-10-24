a1b4b713909797724b7adc68b75c66a9fb03b405
Antti Arrak <antti004@users.noreply.github.com>
2024-10-24 09:25:34 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash shebang
-wget --progress=bar:force:noscroll https://github.com/antti004/Linux/raw/main/.nanorc -O .nanorc
-wget --progress=bar:force:noscroll https://github.com/antti004/Linux/raw/main/.bash_profile -O .bash_profile
-wget --progress=bar:force:noscroll https://github.com/antti004/Linux/raw/main/.tmux.conf -O .tmux.conf
-wget --progress=bar:force:noscroll https://github.com/antti004/Linux/raw/main/install.sh -O install.sh
+wget -q --show-progress https://github.com/antti004/Linux/raw/main/.nanorc -O .nanorc
+wget -q --show-progress https://github.com/antti004/Linux/raw/main/.bash_profile -O .bash_profile
+wget -q --show-progress https://github.com/antti004/Linux/raw/main/.tmux.conf -O .tmux.conf
+wget -q --show-progress https://github.com/antti004/Linux/raw/main/install.sh -O install.sh
 
 mkdir -p ~/.installers
 
-wget --progress=bar:force:noscroll  https://dot.net/v1/dotnet-install.sh -O ~/.installers/dotnet-install.sh
+wget -q --show-progress  https://dot.net/v1/dotnet-install.sh -O ~/.installers/dotnet-install.sh
