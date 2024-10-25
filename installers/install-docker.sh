4a1f5cd5c0baec8860a23457a003f21472033264
kasutaja <kasutaja>
2024-10-25 14:47:18 +0300

    Update

--- installers/install-docker.sh
+++ installers/install-docker.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-curl -fsSL get.docker.com -o get-docker.sh && sh get-docker.sh
+echo Download docker script
+curl -fsSL get.docker.com -o ~/.installers/get-docker.sh
+echo Downloaded
 
+RESULT=$?
+if [ $RESULT -ne 0 ]; then
+  echo "Error!"
+  exit 1
+fi
+
+sh ~/.installers/get-docker.sh
 sudo usermod -aG docker ${USER}
 su - ${USER}
 sudo apt install docker-compose
