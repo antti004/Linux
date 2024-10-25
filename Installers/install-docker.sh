50a23949bc730cbdd6eafcdfe41b805b0f8e2ec4
Antti Arrak <antti004@users.noreply.github.com>
2024-10-25 14:02:31 +0300

    Delete Installers directory

--- Installers/install-docker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-curl -fsSL get.docker.com -o get-docker.sh && sh get-docker.sh
-
-sudo usermod -aG docker ${USER}
-su - ${USER}
-sudo apt install docker-compose
--- Installers/install-dotnet.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-/bin/bash ~/.installers/dotnet-install.sh --version latest
