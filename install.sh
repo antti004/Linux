75a363e97159f543f6f3eacd766f9f8719c03d0c
Antti Arrak <antti004@users.noreply.github.com>
2024-10-25 14:11:48 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash shebang
-echo Version 1.5
+echo Version 1.6
 wget -q --show-progress https://github.com/antti004/Linux/raw/main/.nanorc -O ~/.nanorc
 wget -q --show-progress https://github.com/antti004/Linux/raw/main/.bash_profile -O ~/.bash_profile
 wget -q --show-progress https://github.com/antti004/Linux/raw/main/.tmux.conf -O ~/.tmux.conf
 wget -q --show-progress https://github.com/antti004/Linux/raw/main/install.sh -O ~/install.sh
 
 mkdir -p ~/.installers
-rm -rf ~/.installers/
+rm ~/.installers/*
 
 wget -q --show-progress https://github.com/antti004/Linux/raw/main/installers/install-dotnet.sh -O ~/.installers/install-dotnet.sh
 wget -q --show-progress https://github.com/antti004/Linux/raw/main/installers/install-docker.sh -O ~/.installers/install-docker.sh
