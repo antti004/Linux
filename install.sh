635d351230f8fc38565d6f7331e640a6883234f2
Antti Arrak <antti004@users.noreply.github.com>
2024-10-25 14:32:28 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash shebang
-echo Version 1.6
+echo Version 1.7
 wget -q --show-progress https://github.com/antti004/Linux/raw/main/.nanorc -O ~/.nanorc
 wget -q --show-progress https://github.com/antti004/Linux/raw/main/.bash_profile -O ~/.bash_profile
 wget -q --show-progress https://github.com/antti004/Linux/raw/main/.tmux.conf -O ~/.tmux.conf
 wget -q --show-progress https://github.com/antti004/Linux/raw/main/install.sh -O ~/install.sh
 
+echo ## Create .installer directory
 mkdir -p ~/.installers
+echo ## Delete files in .installer directory
 rm ~/.installers/*
 
 wget -q --show-progress https://github.com/antti004/Linux/raw/main/installers/install-dotnet.sh -O ~/.installers/install-dotnet.sh
