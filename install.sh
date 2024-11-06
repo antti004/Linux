184e87e61abac64e4074c157b2d056f54398d8b1
Antti Arrak <antti004@users.noreply.github.com>
2024-11-06 14:30:39 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash shebang
-echo Version 1.8
+echo Version 1.9
 wget -q --show-progress https://github.com/antti004/Linux/raw/main/.bashrc -O ~/.bashrc
 wget -q --show-progress https://github.com/antti004/Linux/raw/main/.nanorc -O ~/.nanorc
+wget -q --show-progress https://github.com/antti004/Linux/raw/main/.zshrc -O ~/.zshrc
+
 wget -q --show-progress https://github.com/antti004/Linux/raw/main/.bash_profile -O ~/.bash_profile
 wget -q --show-progress https://github.com/antti004/Linux/raw/main/.tmux.conf -O ~/.tmux.conf
 wget -q --show-progress https://github.com/antti004/Linux/raw/main/install.sh -O ~/install.sh
