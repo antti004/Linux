53c5fbc9e9a585027c79649966b7d4042f8c79db
Antti Arrak <antti004@users.noreply.github.com>
2024-11-06 12:30:34 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash shebang
-echo Version 1.7
+echo Version 1.8
+wget -q --show-progress https://github.com/antti004/Linux/raw/main/.bashrc -O ~/.bashrc
 wget -q --show-progress https://github.com/antti004/Linux/raw/main/.nanorc -O ~/.nanorc
 wget -q --show-progress https://github.com/antti004/Linux/raw/main/.bash_profile -O ~/.bash_profile
 wget -q --show-progress https://github.com/antti004/Linux/raw/main/.tmux.conf -O ~/.tmux.conf
 wget -q --show-progress https://github.com/antti004/Linux/raw/main/install.sh -O ~/install.sh
+
 
 echo ## Create .installer directory
 mkdir -p ~/.installers
