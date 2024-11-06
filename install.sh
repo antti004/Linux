f64f769cc1cc888c3b0872470696306ecef96975
Antti Arrak <antti004@users.noreply.github.com>
2024-11-06 12:35:20 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,10 +6,9 @@ wget -q --show-progress https://github.com/antti004/Linux/raw/main/.bash_profile
 wget -q --show-progress https://github.com/antti004/Linux/raw/main/.tmux.conf -O ~/.tmux.conf
 wget -q --show-progress https://github.com/antti004/Linux/raw/main/install.sh -O ~/install.sh
 
-
-echo ## Create .installer directory
+# Create directory .installers if not existsing
 mkdir -p ~/.installers
-echo ## Delete files in .installer directory
+# Delete files from .installers
 rm ~/.installers/*
 
 wget -q --show-progress https://github.com/antti004/Linux/raw/main/installers/install-dotnet.sh -O ~/.installers/install-dotnet.sh
