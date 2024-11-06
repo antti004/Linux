a8016f84e3562038e57b5df037759281ef883843
Antti Arrak <antti004@users.noreply.github.com>
2024-11-06 13:11:35 +0200

    Update first_install.sh

--- first_install.sh
+++ first_install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 clear
+echo Version 0.1
 apt-get update
 apt-get -y dist-upgrade
 apt-get install -y zsh htop vim git tmux apt-transport-https zip unzip software-properties-common multitail
 # File managers
-sudo apt install -y ncdu eza fzf
+sudo apt install -y ncdu eza
+sudo apt install -y httpie
+
 sudo chsh -s $(which zsh) $USER
 
 sh -c "echo Europe/Tallinn > /etc/timezone"
