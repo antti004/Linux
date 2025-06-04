389592d0a0df89a66d4cda27fd2325f31cdcf2ab
Antti Arrak <antti004@users.noreply.github.com>
2025-06-04 10:10:23 +0300

    Update common.sh

--- installers/common.sh
+++ installers/common.sh
@@ -1,5 +1,16 @@
 # 2025-06-04
 ## Created.
-
+VERSION="1.0"
+echo "Version $VERSION"
+echo "Update all"
 apt update
-apt dist-upgrade
+apt -y dist-upgrade
+
+echo "Set region settings"
+sh -c "echo Europe/Tallinn > /etc/timezone"
+rm -f /etc/localtime
+ln -sf /usr/share/zoneinfo/Europe/Tallinn /etc/localtime
+
+echo "Install nessesary"
+apt install -y zsh htop vim git tmux apt-transport-https zip unzip software-properties-common multitail
+
