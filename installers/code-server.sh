ac04e89ebe438e625ee7c6b186fd26c49a1b4204
Antti Arrak <antti004@users.noreply.github.com>
2025-06-05 11:40:08 +0300

    Create code-server.sh

---
+++ installers/code-server.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+curl -fsSL https://code-server.dev/install.sh | sh
+
+# start service
+## sudo systemctl enable --now code-server@$USER
+
+# Configuration
+## config file ~/.config/code-server/config.yaml
