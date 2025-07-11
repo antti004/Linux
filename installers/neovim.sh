295a713c7f274e091415adef13e911806a16bcf4
Antti Arrak <antti004@users.noreply.github.com>
2025-07-11 11:29:51 +0300

    Update neovim.sh

--- installers/neovim.sh
+++ installers/neovim.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo add-apt-repository universe
-sudo apt install fuse # For snap to run
-wget https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
-sudo mkdir /usr/local/bin
-chmod u+x nvim.appimage
-sudo mv nvim.appimage /usr/local/bin/nvim
+sudo apt install fuse  -y # For snap to run
+wget https://github.com/neovim/neovim/releases/download/nightly/nvim-linux-x86_64.appimage
+#sudo mkdir /usr/local/bin
+chmod u+x nvim-linux-x86_64.appimage
+sudo mv nvim-linux-x86_64.appimage /usr/local/bin/nvim
