27b75f2c9e57e0265165e3aab953d3c60896f75b
Antti Arrak <antti004@users.noreply.github.com>
2025-09-29 20:01:15 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash shebang
-VERSION="27"
+VERSION="28"
 url_repo="https://api.github.com/repos/antti004/linux/contents"
 url_installers="https://github.com/antti004/Linux/raw/main/installers"
 
@@ -11,13 +11,13 @@ sudo apt install -y bc coreutils gawk playerctl
 sudo apt autoremove -y
 
 echo "Download installer.sh"
-wget -q --show-progress https://github.com/antti004/Linux/raw/main/install.sh -O ~/install.sh
+wget -q -N --show-progress https://github.com/antti004/Linux/raw/main/install.sh -O ~/install.sh
 
 
 echo "Download dotfiles"
 curl "$url_repo/dotfiles" |jq -r '.[].name' |while IFS= read -r name;
 do
-  wget -q --show-progress "$url_repo/dotfiles/$name" -o ~/$name
+  wget -q -N --show-progress "$url_repo/dotfiles/$name" -o ~/$name
 done
 
 
@@ -30,7 +30,7 @@ rm -f ~/.installers/* 2>/dev/null
 echo "Download installers ..."
 curl "$url_repo/installers" | jq -r '.[].name' | while IFS= read -r name;
 do
-  wget -q --show-progress "$url_installers/$name" -O ~/.installers/$name
+  wget -q -N --show-progress "$url_installers/$name" -O ~/.installers/$name
 done
 
 echo "Finished"
